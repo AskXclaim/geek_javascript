ee8c7d1d25f6a6749ec5084f540212b7db537269
Kolapo Abolarinwa <kola.abolarinwa@gmail.com>
2025-04-02 12:42:57 +0100

    Add prime number checker simple loop solution

--- beginner/prime_number_checker/public/script/index.js
+++ beginner/prime_number_checker/public/script/index.js
@@ -1,2 +1,25 @@
-"use strict";
 console.log("welcome");
+const isNumber = (value) => !Number.isNaN(parseInt(value));
+const convertToInteger = (value) => Math.floor(Math.abs(parseInt(value)));
+const isNumberPrime = (value) => {
+    if (value === 0 || value === 1) {
+        return false;
+    }
+    if (value === 2 || value === 3) {
+        return true;
+    }
+    for (let i = 2; i < value; i++) {
+        if (value % i === 0) {
+            return false;
+        }
+    }
+    return true;
+};
+const isValuePrime = (value) => {
+    if (!isNumber(value)) {
+        return false;
+    }
+    const convertedValue = convertToInteger(value);
+    return isNumberPrime(convertedValue);
+};
+export { isValuePrime };
--- beginner/prime_number_checker/src/index.ts
+++ beginner/prime_number_checker/src/index.ts
@@ -1,1 +1,32 @@
 console.log("welcome");
+
+const isNumber = (value: string) =>
+    !Number.isNaN(parseInt(value))
+
+const convertToInteger = (value: string) =>
+    Math.floor(Math.abs(parseInt(value)));
+
+const isNumberPrime = (value: number) => {
+    if (value === 0 || value === 1) {
+        return false;
+    }
+    if (value === 2 || value === 3) {
+        return true;
+    }
+    for (let i = 2; i < value; i++) {
+        if (value % i === 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+const isValuePrime = (value: string) => {
+    if (!isNumber(value)) {
+        return false;
+    }
+    const convertedValue = convertToInteger(value);
+    return isNumberPrime(convertedValue);
+}
+
+export { isValuePrime };
